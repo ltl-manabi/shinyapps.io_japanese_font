b66fbea504eaf1def7df7d21e38bbfd6c798a71f
Kenta Tanaka <tanaka@mana.bi>
2021-04-26 00:11:10 +0900

    瀬戸フォントのダウンロードURLを修正

--- use_4_font.sh
+++ use_4_font.sh
@@ -36,7 +36,7 @@ cp ./NotoSansCJKjp-hinted/*.otf ~/.fonts/noto
 cp ./NotoSerifCJKjp-hinted/*.otf ~/.fonts/noto
 
 # 瀬戸フォントの設定
-curl -O -L https://ja.osdn.net/projects/setofont/downloads/61995/setofont_v_6_20.zip/
+curl -O -L https://ja.osdn.net/projects/setofont/downloads/61995/setofont_v_6_20.zip
 unzip setofont_v_6_20.zip
 mkdir -p ~/.fonts/seto
 cp ./setofont/*.ttf ~/.fonts/seto
